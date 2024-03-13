a747f491298e272b28700d51ae4967ab8612308b
samimakter96 <samimakter9641@gmail.com>
2024-03-13 23:24:55 +0530

    callback

---
+++ Asynchronous JavaScript/01-callback-2.js
@@ -0,0 +1,14 @@
+function writeLetter(callback) {
+  setTimeout(function() {
+    console.log('Wrote Letter')
+    callback();
+  }, 2000)
+}
+
+function postLetter() {
+  setTimeout(function() {
+    console.log('Letter Posted')
+  }, 1000)
+}
+
+writeLetter(postLetter)
