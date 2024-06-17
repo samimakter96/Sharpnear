2501d3f552cac4ccb164e99b2f9f12be619f2f1e
samim akter <samimakter9641@gmail.com>
2024-06-17 18:59:49 +0530

    fetch api

---
+++ Fetch API/app.js
@@ -0,0 +1,33 @@
+const URL = "https://cat-fact.herokuapp.com/facts";
+
+
+const factPara = document.querySelector('#fact');
+const btn = document.querySelector('#btn');
+
+
+
+const getFacts = async () => {
+  console.log('getting data ...');
+  let response = await fetch(URL);
+  console.log(response);  // JSON format
+  let data = await response.json();
+  console.log(data[0].text);
+  factPara.innerText = data[1].text;
+}
+
+
+// using promise
+
+// function getFacts() {
+//   fetch(URL)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       factPara.innerText = data[1].text;
+//   })
+// }
+
+
+btn.addEventListener('click', getFacts);
